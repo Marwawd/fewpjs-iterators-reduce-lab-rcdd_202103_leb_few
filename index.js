d169a411fb4e00979d1d308a294c6a53a8a2a2b0
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-29 22:44:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries=function(g){
+  return g.reduce((a,c)=> a+c);
+
+}
+let res=totalBatteries(batteryBatches);
